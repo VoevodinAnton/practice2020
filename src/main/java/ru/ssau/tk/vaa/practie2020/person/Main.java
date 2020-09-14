823ce92cf5572a29543c2dcda497f2ffd85a1d02
Anton <voevodin_anton_2000@mail.ru>
2020-09-14 07:38:43 +0400

    Изменены названия переменных ссылающихся на класс Person

--- src/main/java/ru/ssau/tk/vaa/practie2020/person/Main.java
+++ src/main/java/ru/ssau/tk/vaa/practie2020/person/Main.java
@@ -3,19 +3,19 @@ package ru.ssau.tk.vaa.practie2020.person;
 public class Main extends Person {
 
     public static void main(String[] args) {
-        Person number1 = new Person();
-        number1.setFirstName("Anton");
-        number1.setLastName("Voevodin");
-        number1.setPassportID(12345);
+        Person anton = new Person();
+        anton.setFirstName("Anton");
+        anton.setLastName("Voevodin");
+        anton.setPassportID(12345);
 
-        Person number2 = new Person();
-        number2.setFirstName("Pavel");
-        number2.setLastName("Ivanov");
-        number2.setPassportID(6789);
+        Person pavel = new Person();
+        pavel.setFirstName("Pavel");
+        pavel.setLastName("Ivanov");
+        pavel.setPassportID(6789);
 
-        Person number3 = new Person("Kirill","Petrov", 2468);
-        Person number4 = new Person("Vika","Semenova");
-        Person number5 = new Person(1357);
-        Person number6 = new Person();
+        Person kirill = new Person("Kirill","Petrov", 2468);
+        Person vika = new Person("Vika","Semenova");
+        Person misha = new Person(1357);
+        Person sonya = new Person();
     }
 }

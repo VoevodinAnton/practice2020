22d823ff40d905dae698301554546136fc2e67bc
Anton <voevodin_anton_2000@mail.ru>
2020-09-15 18:27:40 +0400

    В класс Points добавлены методы для вычисления скалярного и векторного произведения векторов

--- src/main/java/ru/ssau/tk/vaa/practie2020/point/Main.java
+++ src/main/java/ru/ssau/tk/vaa/practie2020/point/Main.java
@@ -5,6 +5,7 @@ public class Main {
         Point point123 = new Point(1, 2, 3);
         Point point398 = new Point(0, 9, 8);
         Point point473 = new Point(4, 7, 3);
-        Point pointDivide = Points.divide(point123, point398);
+        Point point = Points.vectorProduct(point123,point398);
+        System.out.println(point.y);
     }
 }
--- src/main/java/ru/ssau/tk/vaa/practie2020/point/Points.java
+++ src/main/java/ru/ssau/tk/vaa/practie2020/point/Points.java
@@ -27,4 +27,20 @@ public class Points {
     public static Point enlarge(Point a, double number) {
         return new Point(a.x * number, a.y * number, a.z * number);
     }
+
+    public static Point opposite(Point a) {
+        return new Point(-a.x, -a.y, -a.z);
+    }
+
+    public static Point inverse(Point a) {
+        return new Point(1 / a.x, 1 / a.y, 1 / a.z);
+    }
+
+    public static double scalarProduct(Point a, Point b) {
+        return a.x * b.x + a.y * b.y + a.z * b.z;
+    }
+
+    public static Point vectorProduct(Point a, Point b) {
+        return new Point(a.y * b.z - a.z * b.y, -(a.x * b.z - a.z * b.x), a.x * b.y - a.y * b.z);
+    }
 }

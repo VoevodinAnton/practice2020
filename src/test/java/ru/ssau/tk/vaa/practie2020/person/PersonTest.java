daabe58a219021d6e247f2bfd95c5818bd698149
Anton <voevodin_anton_2000@mail.ru>
2020-09-20 17:09:24 +0400

    Добавлен тест для конструкторов класса Person

--- src/test/java/ru/ssau/tk/vaa/practie2020/person/PersonTest.java
+++ src/test/java/ru/ssau/tk/vaa/practie2020/person/PersonTest.java
@@ -27,4 +27,24 @@ public class PersonTest {
         assertEquals(person2.getPassportID(), 1);
 
     }
+
+    @Test
+    public void testConstructor(){
+        Person person1 = new Person("Anton", "Voevodin");
+        Person person2 = new Person(1234);
+        Person person3 = new Person("Pavel", "Ivanov", 1);
+
+        assertEquals(person1.getFirstName(), "Anton");
+        assertEquals(person1.getLastName(), "Voevodin");
+        assertEquals(person1.getPassportID(), 0);
+
+        assertNull(person2.getFirstName());
+        assertNull(person2.getLastName());
+        assertEquals(person2.getPassportID(), 1234);
+
+        assertEquals(person3.getFirstName(), "Pavel");
+        assertEquals(person3.getLastName(), "Ivanov");
+        assertEquals(person3.getPassportID(), 1);
+
+    }
 }

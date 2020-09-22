368e464f52892d60d84b94a9988257a211e49f60
Anton <voevodin_anton_2000@mail.ru>
2020-09-23 00:04:24 +0400

    В maven добавлена библиотека telegrambots, создан класс TelegramBot

---
+++ src/main/java/ru/ssau/tk/vaa/practie2020/TelegramBot/TelegramBot.java
@@ -0,0 +1,34 @@
+package ru.ssau.tk.vaa.practie2020.TelegramBot;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+
+public class TelegramBot extends TelegramLongPollingBot {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new TelegramBot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+}

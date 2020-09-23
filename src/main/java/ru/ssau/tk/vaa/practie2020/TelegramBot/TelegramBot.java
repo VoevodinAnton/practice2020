231282eff4887bd8117ea778b53ee4dcf563ecb2
Anton <voevodin_anton_2000@mail.ru>
2020-09-23 21:19:03 +0400

    Создан бот-расписание, добавлено тестовое расписание на понедельник

--- src/main/java/ru/ssau/tk/vaa/practie2020/TelegramBot/TelegramBot.java
+++ src/main/java/ru/ssau/tk/vaa/practie2020/TelegramBot/TelegramBot.java
@@ -2,8 +2,11 @@ package ru.ssau.tk.vaa.practie2020.TelegramBot;
 
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
 
 public class TelegramBot extends TelegramLongPollingBot {
@@ -17,18 +20,47 @@ public class TelegramBot extends TelegramLongPollingBot {
         }
     }
 
+    public void SendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     @Override
     public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            switch (message.getText()) {
+                case "/help":
+                    SendMsg(message, "help");
+                    break;
+                case "/setting":
+                    SendMsg(message, "setting");
+                    break;
+                case "/Monday":
+                    SendMsg(message, "start 9:45\n\nPhysic 362-5\nPhysic 362-5\nPhysic 362-5\nEnglish\nEnglish");
+                    break;
 
+                default:
+            }
+        }
     }
 
     @Override
     public String getBotUsername() {
-        return null;
+        return "Time11_Bot";
     }
 
     @Override
     public String getBotToken() {
-        return null;
+        return "1056002621:AAEbnynWZUFhHV_kWk6GiAvQGjInyqivDKE";
     }
 }

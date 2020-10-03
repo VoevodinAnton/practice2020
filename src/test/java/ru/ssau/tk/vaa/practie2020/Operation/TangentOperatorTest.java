91c83502982fea964f054d5a2f78ecb5438bc6e0
Anton <voevodin_anton_2000@mail.ru>
2020-10-03 17:18:14 +0400

    added tests for a tangentOperator

--- src/test/java/ru/ssau/tk/vaa/practie2020/Operation/TangentOperatorTest.java
+++ src/test/java/ru/ssau/tk/vaa/practie2020/Operation/TangentOperatorTest.java
@@ -2,6 +2,8 @@ package ru.ssau.tk.vaa.practie2020.Operation;
 
 import org.testng.annotations.Test;
 
+import javax.validation.constraints.Null;
+
 import static org.testng.Assert.*;
 
 public class TangentOperatorTest {
@@ -12,6 +14,27 @@ public class TangentOperatorTest {
         TangentOperator operator = new TangentOperator();
 
         assertEquals(operator.apply(Math.PI), 0.0, ACCURACY);
+    }
+
+    @Test
+    public void applyInfinityTest(){
+        TangentOperator operator = new TangentOperator();
+
+        assertEquals(operator.apply(Math.PI/2), 1.633123935319537E16);
+        assertEquals(operator.apply(-Math.PI/2), -1.633123935319537E16);
+    }
+
+    public void applyNanTest(){
+        TangentOperator operator = new TangentOperator();
+
+        assertTrue(Double.isNaN(operator.apply(Double.NaN)));
+    }
+
+    @Test
+    public void applyTripleTest(){
+        TangentOperator operator = new TangentOperator();
+
+        assertEquals(operator.applyTriple(Math.PI), 0.0, ACCURACY);
     }
 
 }
